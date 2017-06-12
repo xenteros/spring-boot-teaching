94eaa905cdda3ca825523b9d8bf01a3aab315493
agurgul <adam.gurgul@intenso.pl>
2017-06-12 13:05:37 +0200

    HelloWorld
    * Added @RestController annotation,
    * Added helloWorld method,
    * Added /hello mapping,
    * Added application.properties to start on different port

--- src/main/java/com/github/xenteros/Application.java
+++ src/main/java/com/github/xenteros/Application.java
@@ -2,12 +2,20 @@ package com.github.xenteros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by agurgul on 12.06.2017.
  */
+@RestController
 @SpringBootApplication
 public class Application {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
