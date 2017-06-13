c0850d96df6b0252ac7e6b0d1a32c77ea4482db8
xenteros <adam@gurgul.com.pl>
2017-06-13 16:32:34 +0200

    Present different way of mapping (#4)
    * Added @PathVariable example,
    * Added @RequestParam example.

--- src/main/java/com/github/xenteros/controller/HelloWorldController.java
+++ src/main/java/com/github/xenteros/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.github.xenteros.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -12,9 +14,18 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/api")
 public class HelloWorldController {
 
-    @RequestMapping(method = GET, value = "/hello")
+    @RequestMapping(method = GET, value = "/helloWorld")
     public String helloWorld() {
         return "Hello World";
     }
 
+    @RequestMapping(method = GET, value = "/hello/{name}")
+    public String helloName(@PathVariable("name") String name) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(method = GET, value = "/hello")
+    public String helloNameParam(@RequestParam("name") String name) {
+        return "Hello " + name;
+    }
 }
